9339c0c9cdd1f78ae1b92005acf173f0e2b0cb60
Jose Nicolas Acosta <Jose.Acosta2@halliburton.com>
2024-02-29 17:27:08 -0500

    JSON(50) con contraseñas modificado

--- codigo2.py
+++ codigo2.py
@@ -7,11 +7,13 @@ def generate_random_data():
         name = ''.join(random.choice(string.ascii_letters) for _ in range(10))
         domain = random.choice(['hotmail.com', 'gmail.com', 'yahoo.com','aol.com'])
         email = f"{name}@{domain}"
-        passord_length = random.randint(5,12)
-        password = ''.join(random.choice(string.ascii_letters + string.digits))
+        password_length = random.randint(5,12)
+        chars = string.ascii_letters + string.digits
+
+        password = ''.join(random.choice(chars) for _ in range(password_length))
         return {"name": name, "email": email, "password": password}
 
-data = [generate_random_data() for _ in range(500000)]
+data = [generate_random_data() for _ in range(50)]
 
 with open('data.json','w') as json_file:
     json.dump(data, json_file, indent=2)
